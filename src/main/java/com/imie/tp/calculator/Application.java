314ea715ad76740e1b8ab808cab78f8ba22a7c3b
BenBenDev <benben.dev@gmail.com>
2018-01-26 11:28:20 +0100

    first pull - init

--- src/main/java/com/imie/tp/calculator/Application.java
+++ src/main/java/com/imie/tp/calculator/Application.java
@@ -4,6 +4,8 @@ public class Application {
 
     public static void main(String[] args) {
 
+        boolean isEnd = false;
+        do {
         // Process...
         // Display & Ask "Type of Operation ":
         //               - 1 : Addition
@@ -12,7 +14,13 @@ public class Application {
         //               - 4 : Multiplication
         //               - 5 : Display History
         //               - 9 : Quit
-
+            System.out.println("Type of Operation :");
+            System.out.println("- 1 : Addition");
+            System.out.println("- 2 : Subtraction");
+            System.out.println("- 3 : Diviside");
+            System.out.println("- 4 : Multiplication");
+            System.out.println("- 5 : Display History");
+            System.out.println("- 9 : Quit");
         // If enter 1
         // Display & Ask "Enter Value a : "
         // Display & Ask "Enter Value b : "
@@ -23,7 +31,8 @@ public class Application {
         // Display last calculate :
         //    1 + 1 = 2
         //    2 * 3 = 6
-
+            isEnd = true;
+        } while (isEnd == false);
         // If enter 9 => Quit application
 
         //TODO
